884ca9949af47d8ff866bd4a05e927907f0afe96
Gian Luca Toni <gianluca.toni@gmail.com>
2026-03-02 15:53:34 +0100

    Fix sulla Capital Letter const Player = require("../models/player.js");

--- routes/potenze.js
+++ routes/potenze.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Player = require("../models/Player.js");
+const Player = require("../models/player.js");
 const { requireAuth, requireLevel } = require("../middleware/auth");
 
 const router = express.Router();
