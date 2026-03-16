251f37b6163f70c63d2e8de6d6bf7356d4c9ccdf
Gian Luca Toni <gianluca.toni@gmail.com>
2026-03-16 17:15:53 +0100

    Fix per Bold

--- routes/potenze.js
+++ routes/potenze.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const XLSX = require("xlsx");
+const XLSX = require("xlsx-js-style");
 const Player = require("../models/player.js"); // p minuscola
 const { requireAuth, requireLevel } = require("../middleware/auth");
 const { buildPlayerDetails, createEventLog } = require("../utils/eventLog");
